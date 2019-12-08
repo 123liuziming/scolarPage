61a165d248c7d3793a10a5609cc27e76d961e56d
Vian Chen <imvianchen@outlook.com>
2019-12-08 16:16:33 +0800

    Removed unnecessary components

--- src/components/pdf.vue
+++ src/components/pdf.vue
@@ -1,33 +1,26 @@
 <template>
   <div>
-    <object class="pdfs"
-      data="">
-    </object>  
+    <object class="pdfs" data="" />
   </div>
 </template>
+
 <script>
-import pdf from "vue-pdf";
 export default {
-  components: { pdf },
   data() {
     return {
       pdfSrc: "//static.aminer.org/pdf/PDF/000/000/097/database_security.pdf" // pdf文件地址
     };
   },
-  mounted(){
+  mounted() {
     var _this = this;
-    let pdfs = document.getElementsByClassName("pdfs")
-    pdfs[0].setAttribute("data",_this.pdfSrc)
-  },
-  methods: {
-    download() {
-      alert("确定要下载吗？");
-    }
+    let pdfs = document.getElementsByClassName("pdfs");
+    pdfs[0].setAttribute("data", _this.pdfSrc);
   }
 };
 </script>
+
 <style scoped>
-.pdfs{
+.pdfs {
   text-align: center;
   width: 100%;
   height: 45vw;
